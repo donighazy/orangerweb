4d3ff59ac6ba365b5c96da88e7e748667c7fa63b
donighazy <72773525+donighazy@users.noreply.github.com>
2020-10-23 00:12:40 +0700

    Update refmaster.php

--- admin/refmaster.php
+++ admin/refmaster.php
@@ -7,7 +7,7 @@
 	$query = mysqli_query($koneksi, "SELECT * FROM ref_produk_pospay WHERE id_produk='$id' and jenis= '$jenis'");
 	$data  = mysqli_fetch_assoc($query);
 	$admin = $data['admin'];
-	echo $admin;
+	return $admin;
   
   
   
